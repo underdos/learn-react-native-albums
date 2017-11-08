5a46ffbff451e84edf3955f3fbc382fcffc4f9ea
Kusnadi <kus.underdos@gmail.com>
2017-11-08 23:46:13 +0700

    First Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <Text>Some Text</Text>
+    );
+  }
+
+}
+
